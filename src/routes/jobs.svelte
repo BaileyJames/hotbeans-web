2ed900d85c442c38e4093601dee1730677a0b379
root <root@BaileysWindowsMachine.localdomain>
2022-04-04 14:08:07 +0100

    add more stuff idk

--- src/routes/jobs.svelte
+++ src/routes/jobs.svelte
@@ -1,3 +1,50 @@
-<div class="wrapper">
-    <div class="section"></div>
+<div class="wrapper container">
+    <div class="jobs">
+        <div class="section">
+            <h1 class="center">Available positions</h1>
+        <h3 class="center">View our currently available job roles</h3>
+        <div class="job-cards container">
+            <div class="job-card">
+                <img src="web designer2.svg" alt="web-design">
+                <h2 class="center">Web Designer</h2>
+                <p class="info">Work directly with the clients to create mock designs, including wireframes and layouts to fit the clients needs.</p>
+                <p class="more-info"><a href="/">More info</a></p>
+            </div>
+            <div class="job-card">
+                <img src="web designer.svg" alt="web-design">
+                <h2 class="center">Front-end developer</h2>
+                <p class="info">Produce webpages based on client feedback and designs to get what's right for the client</p>
+                <p class="more-info"><a href="/">More info</a></p>
+
+            </div>
+            <div class="job-card">
+                <img src="web designer.svg" alt="web-design">
+                <h2 class="center">Back-end developer</h2>
+                <p class="info">Work behind the scenes, connecting the frontend to servers in bigger projects.</p>
+                <p class="more-info"><a href="/">More info</a></p>
+            </div>
+            <div class="job-card">
+                <img src="sysadmin.svg" alt="web-design">
+                <h2 class="center">System Administrator</h2>
+                <p class="info">Be in control of the company hardware, ensuring reliable operations. It's all about "up-time" at HotBeans.</p>
+                <p class="more-info"><a href="/">More info</a></p>
+
+            </div>
+            <div class="job-card">
+                <img src="seo.svg" alt="web-design">
+                <h2 class="center">SEO Specialist</h2>
+                <p class="info">Ensure that our clients are always ahead of the game. You'll be helping the clients rank high in the alrogithms!</p>
+                <p class="more-info"><a href="/">More info</a></p>
+            </div>
+            <div class="job-card">
+                <img src="analyst.svg" alt="web-design">
+                <h2 class="center">Business Analyst</h2>
+                <p class="info">You'll be analysing the clients business to ensure that the project is feasible and worthwhile.</p>
+                <p class="more-info"><a href="/">More info</a></p>
+            </div>
+
+        </div>
+        </div>
+        
+    </div>
 </div>
