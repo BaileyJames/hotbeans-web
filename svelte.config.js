0965647bb89e2dab613d7ed750ed50a60d5bba4f
BaileyJames <baileyheyworth@gmail.com>
2022-04-05 20:32:55 +0100

    add and configure svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,14 @@
-// // svelte.config.js
-// import adapter from '@sveltejs/adapter-static';
+/** @type {import('@sveltejs/kit').Config} */
 
-// export default {
-// 	kit: {
-// 		adapter: adapter({
-// 			// default options are shown
-// 			pages: 'build',
-// 			assets: 'build',
-// 			fallback: null,
-// 			precompress: false
-// 		}),
+import adapter from '@sveltejs/adapter-static';
 
-// 		prerender: {
-// 			// This can be false if you're using a fallback (i.e. SPA mode)
-// 			default: true
-// 		}
-// 	}
-// };
-// svelte.config.js
-import adapter from '@sveltejs/adapter-node';
-
-export default {
-	kit: {
-		adapter: adapter()
-	}
+const config = {
+    kit: {
+        adapter: adapter(),
+        prerender: {
+            default: true,
+        },
+    }
 };
+
+export default config;
